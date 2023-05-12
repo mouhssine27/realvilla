2808e260a27abe1285164751d9a966c383511ffb
ilhammihiri <ilhams.ihiri02@gmail.com>
2023-05-12 18:15:17 +0200

    adminauthcontroller

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\controllers\AnnonceController;
 use App\Http\controllers\ContactController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
+
 
 
 
@@ -58,8 +57,3 @@ Route::get('property_list/{id}', [AnnonceController::class, 'propertylistID']);
 Route::get('details/{name}/{id}', [AnnonceController::class, 'details']);
 
 Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
-
-Route::get('/admin', [LoginController::class, 'showAdminLoginForm'])->name('admin.login-view');
-Route::post('/admin', [LoginController::class, 'adminLogin'])->name('admin.login');
-Route::get('/admin/logout', [LoginController::class, 'Logout'])->name('admin.logoutt');
-Route::get('/admin/dashboard', [LoginController::class, 'dashboard'])->middleware('auth:admin');
